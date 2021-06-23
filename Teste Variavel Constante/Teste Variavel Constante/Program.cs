0e9c3226ce8d6c78dc34ceba66c2c3800f62e3f9
Diego Oliveira <86024091+OliverDiego10@users.noreply.github.com>
2021-06-23 15:03:20 -0300

    Teste Variavel Constante

---
+++ Teste Variavel Constante/Teste Variavel Constante/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Teste_Variavel_Constante
+{
+    class Program
+    {
+        private static object argumentos;
+
+        static void Main(string[] args)
+        {
+            const string constante = "uma constante que não muda"; 
+
+            if(argumentos.length == 0)
+            {
+                Console.WriteLine("Favor informar um número inteiro");
+                return;
+            }
+            int inteiro;
+            bool teste = int.TryParse(argumentos[0], out inteiro);
+
+            if(teste == false
+                {
+                Console.WriteLine("o argumento informado não é um numero inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("o número informado foi: " + inteiro);
+        }
+    }
+}
