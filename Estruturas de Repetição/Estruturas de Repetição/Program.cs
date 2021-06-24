a5e9a0d3d51e607d28fbc483b5bafe9a0949726d
Diego Oliveira <86024091+OliverDiego10@users.noreply.github.com>
2021-06-24 14:15:53 -0300

    Estruturas de Repetição

---
+++ Estruturas de Repetição/Estruturas de Repetição/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Estruturas_de_Repetição
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+        {
+            if(argumentos.Length == 0)
+            {
+                Console.WriteLine("Favor informar palavras no prograama");
+                return;
+            }
+            //for
+            for (var indice = 0; indice < argumentos.Length; indice++)
+            {
+                Console.WriteLine($"Estrutura for, indice e valor lido: {indice}, {argumentos[indice]}");
+            }
+            //while
+            var argumentosLidos = 0;
+            while(argumentosLidos < argumentos.Length)
+            {
+                Console.WriteLine($"Estrutura while, argumentosLidos e valor lido: {argumentosLidos}, {argumentos[argumentosLidos]}");
+                argumentosLidos++;
+            }
+
+            //do while
+            var loopsEfetuados = 0;
+            do
+            {
+                Console.WriteLine($"Estrutura do while, loops efetuados: {loopsEfetuados}, valor lido {argumentos[loopsEfetuados]}");
+                loopsEfetuados++;
+            } while (loopsEfetuados < argumentos.Length);
+
+            //foreach
+            foreach(var argumento in argumentos)
+            {
+                Console.WriteLine($"Estrutura foreach, valor lido: {argumento}");
+            }
+        }
+    }
+}
