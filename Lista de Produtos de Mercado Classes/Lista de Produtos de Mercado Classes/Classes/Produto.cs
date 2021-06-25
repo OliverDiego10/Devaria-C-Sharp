c825b1caf75859c0dccd162499e701cf01dccfdd
Diego Oliveira <86024091+OliverDiego10@users.noreply.github.com>
2021-06-25 19:31:02 -0300

    ListaProdutoClassesClasses

---
+++ Lista de Produtos de Mercado Classes/Lista de Produtos de Mercado Classes/Classes/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Produtos_de_Mercado_Classes.Classes
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public string ExibirDadosProduto()
+        {
+            return $"Produto : {Nome} com valor de R$ {Preco}";
+
+        }
+
+    }
+}
